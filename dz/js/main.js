b441a9bca605e4fab4d85bdd469b239c8868bb0e
andre563 <69888020+andre563@users.noreply.github.com>
2020-08-30 18:06:29 +0300

    Коментарії

--- dz/js/main.js
+++ dz/js/main.js
@@ -1,38 +1,40 @@
 /*-------------------------------------Задача 1-----------------------------------------------------------------------*/
 class CheckMul {
-	constructor(firstNumber) {
-		this.number = firstNumber
-		this.numberCorrect = 0
-		this.numberIncorrect = 0
+	constructor(firstNumber) {//Функція конструктор
+		this.number = firstNumber//Створюєм властивість "this.number" і записуєм занчення firstNumber
+		this.numberCorrect = 0	//Створюєм властивість "this.numberCorrect" і записуєм занчення занчення 0
+		this.numberIncorrect = 0 //Створюєм властивість "this.numberIncorrect" і записуєм занчення занчення 0
 	}
-	render(containerId) {
-		const container = document.getElementById(containerId)
-		container.textContent = ''
+	
+//Prototype--------------------------------------------------------------------------------------------------------------------
+	render(containerId) { //Функція створення розмітки на сторінці
+		const container = document.getElementById(containerId) //Зберігаємо адрес контейнера в констані "const"
+		container.textContent = '' // Чистимо контейнер
 		// Создаєм і вставляєм пустий div для запису прикладів. Також зберігаєм його адресу в "this.divId"
-		let div = document.createElement('div')
-		this.divId = div
-		container.appendChild(div)
+		let div = document.createElement('div') // Создаєм елемен "div"
+		this.divId = div //Записуєм адерес "div" в "this.divId"
+		container.appendChild(div) //Вставляєм елемент "div" в "container"
 		//----------------------------------------------------
 
 		//Создаєм кнопку для генерування прикладів і виводу їх на екран
-		div = document.createElement('div')
-		let btn = document.createElement('button')
-		btn.innerText = "Згенерувати приклад"
-		btn.onclick = this.getRandomExample.bind(this)
-		div.appendChild(btn)
-		container.appendChild(div)
+		div = document.createElement('div')//Перезаписуєм змінну "div" адресом нового елемента "div"
+		let btn = document.createElement('button') // Создаєм елемент "button" і записуєм його адрес в змінну "btn"
+		btn.innerText = "Згенерувати приклад" // Вставляєм текст в "button"
+		btn.onclick = this.printRandomExample.bind(this) // Підключаєм функцію на подію "onclick"
+		div.appendChild(btn) //Вставляєм елемент "button" в "div"
+		container.appendChild(div)//Вставляєм елемент "div" в "container"
 		//----------------------------------------------------
 
 		//Создаєм інпут для запису відповідей користувача
-		div = document.createElement('div')
-		let label = document.createElement('label')
-		label.innerText = 'Ваша відповідь'
-		const input = document.createElement('input')
-		input.setAttribute('type', 'number')
-		label.appendChild(input)
-		this.numberInput = input //зберегли інпут як ід
-		div.appendChild(label)
-		container.appendChild(div)
+		div = document.createElement('div') //Перезаписуєм змінну "div" адресом нового елемента "div"
+		let label = document.createElement('label')// Создаєм елемент "label" і записуєм його адрес в змінну "label"
+		label.innerText = 'Ваша відповідь'// Вставляєм текст в "label"
+		const input = document.createElement('input')// Создаєм елемент "input" і записуєм його адрес в змінну "input"
+		input.setAttribute('type', 'number')// Міняєм значення атрибуту "type" на "number"
+		label.appendChild(input)//Вставляєм елемент "input" в "label"
+		this.numberInput = input //Зберегли адрес елемента "input" в змінній "this.numberInput"
+		div.appendChild(label) //Вставляєм елемент "label" в "div"
+		container.appendChild(div)//Вставляєм елемент "div" в "container"
 		//----------------------------------------------------
 
 		//Создаєм кнопку для перевірки відповіді
@@ -58,25 +60,25 @@ class CheckMul {
 		container.appendChild(this.messageDiv)
 		//----------------------------------------------------
 	}
-	getRandomExample() { //Функція для створення приклада
-		this.secondNumber = Math.floor(Math.random() * 10)
-		this.divId.innerText = `${this.number}*${this.secondNumber}`
+	printRandomExample() { //Функція для створення приклада
+		this.secondNumber = Math.floor(Math.random() * 10) //Зберігаєм друге число в змінній "this.secondNumber". 
+		this.divId.innerText = `${this.number}*${this.secondNumber}` // Виводим приклад на екран
 	}
 	checkValue() { //Функція для перевірки розвязка
-		let value = parseInt(this.numberInput.value)
-		if (value === this.secondNumber * this.number) {
-			this.numberCorrect++
-				alert('Correct')
+		let value = parseInt(this.numberInput.value) // Отримуємо відповідь користувача і записуєм її в "value"
+		if (value === this.secondNumber * this.number) { //Перевіряєм  чи відповідь правильна чи ні
+			this.numberCorrect++ // Збільшуєм кількість правильних відповідей на 1
+				alert('Correct') //Вивобим повідомлення про правильну відповідь
 		} else {
-			this.numberIncorrect++
-				alert('Incorrect')
+			this.numberIncorrect++ //Збільшуєм кількість не правильних відповідей на 1
+				alert('Incorrect') //Вивобим повідомлення про не правильну відповідь
 		}
-		this.numberInput.value = ''
-
+		this.numberInput.value = '' //Чистимо Input для відповідей
+		this.printRandomExample()	//Виводим наступний приклад
 	}
 	printResult() { //Функція для виводу речення
-		this.messageDiv.innerText = `Ви правинильно відповіли ${this.numberCorrect} р. і неправильно ${this.numberIncorrect} р.`
-	}
+		this.messageDiv.innerText = `Ви правинильно відповіли ${this.numberCorrect} р. і неправильно ${this.numberIncorrect} р.` //Виводим кількість правильних та неправильних відповідей
+//----------------------------------------------------------------------------------------------------------------------	
 }
 /*--------------------------------------------------------------------------------------------------------------------*/
 
@@ -86,6 +88,7 @@ class OrderPrice {
 		this.price = price
 		this.sizeDiscounts = 5
 	}
+//Prototype--------------------------------------------------------------------------------------------------------------------
 	render(containerId) {
 		const container = document.getElementById(containerId)
 		container.textContent = ''
@@ -117,24 +120,25 @@ class OrderPrice {
 		//----------------------------------------------------
 	}
 	printSum() { //Функція для виводу сими результату
-		let numberDays = parseInt(this.numberInput.value)
+		let numberDays = parseInt(this.numberInput.value) //Отримуєм кількість днів 
 		let totalSum
-		alert(`${numberDays}-${this.price}`)
-		if (numberDays <= 10)
-			totalSum = this.price * numberDays
+		if (numberDays <= 10)//Перевіряєм чи кількість днів менша рівна за 10
+			totalSum = this.price * numberDays //Вираховуєм суму
 		else
-			totalSum = (numberDays - 10) * (this.price - (this.price / 100 * 5)) + 10 * this.price
+			totalSum = (numberDays - 10) * (this.price - (this.price / 100 * this.sizeDiscounts)) + 10 * this.price //Вираховуєм суму
 
-		this.messageDiv.innerText = `До оплати ${totalSum}грн`
+		this.messageDiv.innerText = `До оплати ${totalSum}грн` //Виводим суму в "div" з адресою що збережена в "this.messageDiv"
 	}
+//----------------------------------------------------------------------------------------------------------------------	
 }
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 /*-------------------------------------Задача 3-----------------------------------------------------------------------*/
 class RandomBaner {
 	constructor() {
-		this.objectArray = []
+		this.objectArray = [] // Сворюємо пустий масив "this.objectArray"
 	}
+//Prototype--------------------------------------------------------------------------------------------------------------------
 	render(containerId) {
 		const container = document.getElementById(containerId)
 		container.textContent = ''
@@ -144,7 +148,7 @@ class RandomBaner {
 		label.innerText = 'Введіть URL сайта'
 		let input = document.createElement('input')
 		label.appendChild(input)
-		this.numberInput = input //зберегли інпут як ід
+		this.numberInput = input 
 		div.appendChild(label)
 		container.appendChild(div)
 		//----------------------------------------------------
@@ -155,7 +159,7 @@ class RandomBaner {
 		label.innerText = 'Введіть URL картинки'
 		input = document.createElement('input')
 		label.appendChild(input)
-		this.numberInput2 = input //зберегли інпут як ід
+		this.numberInput2 = input 
 		div.appendChild(label)
 		container.appendChild(div)
 		//----------------------------------------------------
@@ -182,21 +186,21 @@ class RandomBaner {
 		container.appendChild(this.messageDiv)
 		//----------------------------------------------------
 	}
-	pushObject() { //Функція для виводу сими результату
+	pushObject() { //Функція для запису обєкта "Банер" в масив
 		this.objectArray.push({
-			urlImg: this.numberInput2.value,
-			urlSite: this.numberInput.value
+			urlImg: this.numberInput2.value,//Зберізаєм url картинки в urlImg
+			urlSite: this.numberInput.value //Зберізаєм url сайта в urlSite
 		})
-		this.numberInput.value = ''
-		this.numberInput2.value = ''
-		this.messageDiv.innerText = 'Банер збережено'
+		this.numberInput.value = '' //Чистимо Input
+		this.numberInput2.value = ''//Чистимо Input
+		this.messageDiv.innerText = 'Банер збережено' //Виводим службове повідомлення про збереження Банера
 	}
-	printRandomBaner() {
-		let randomIndex = Math.floor(Math.random() * this.objectArray.length)
-		this.messageDiv.innerHTML = `<a href="${this.objectArray[randomIndex].urlSite}" target="_blank">
-					<img src="${this.objectArray[randomIndex].urlImg}"></a>`
+	printRandomBaner() { //Функція виводу випадкового банеру 
+		let randomIndex = Math.floor(Math.random() * this.objectArray.length) // Отримуємо випадковий індех
+		this.messageDiv.innerHTML = `<a href="${this.objectArray[randomIndex].urlSite}" target="_blank"> 
+					<img src="${this.objectArray[randomIndex].urlImg}"></a>`//Виводимо сформований банер
 	}
-
+//----------------------------------------------------------------------------------------------------------------------	
 }
 /*--------------------------------------------------------------------------------------------------------------------*/
 
@@ -204,12 +208,12 @@ class RandomBaner {
 window.onload = function() {
 	//Задача 1
 	let btn = document.getElementById('start')
-	btn.onclick = function start() { //Вводем перше число з інпута при нажатії на кнопку. 
+	btn.onclick = function () { //Вводим перше число з інпута при нажатії на кнопку. 
 		let inp = document.getElementById('firstNumber')
 		let firstNumber = parseInt(inp.value)
+		let check = new CheckMul(firstNumber) //Створюєм обєкт "check" за допомогою конструктора "CheckMul"
 
-		let check = new CheckMul(firstNumber)
-		check.render('container')
+		check.render('container')//Запускаєм функцію "render"
 	}
 	//-------------------------------------------------------------------------------------
 
